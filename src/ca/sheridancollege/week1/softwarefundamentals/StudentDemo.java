5b79bebcc3992144ec7bed3c96d833673fa56bde
Essa OG <Essa OG@DESKTOP-P8U7JG2>
2019-01-25 16:44:03 -0500

    how are you boi

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentDemo.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentDemo.java
@@ -13,6 +13,7 @@ public class StudentDemo
 {
   private String studentID;
   private String name;
+  private String program;
   
   /**
    * A constructor to pass in the given ID and then name
@@ -56,6 +57,20 @@ public class StudentDemo
      */
     public void setName(String givenName) {
         name = givenName;
+    }
+
+    /**
+     * @return the program
+     */
+    public String getProgram() {
+        return program;
+    }
+
+    /**
+     * @param program the program to set
+     */
+    public void setProgram(String program) {
+        this.program = program;
     }
     
 }
